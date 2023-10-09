dc151d1dd6484ed84c52f6448e8612b054c7b811
garvmehta <garvmehta7701@gmail.com>
2023-10-09 21:30:34 +0530

    init app

---
+++ lib/screens/placeOrder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlaceOrder extends StatefulWidget {
+  const PlaceOrder({super.key});
+
+  @override
+  State<PlaceOrder> createState() => _PlaceOrderState();
+}
+
+class _PlaceOrderState extends State<PlaceOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("place Order");
+  }
+}
